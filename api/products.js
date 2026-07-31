b2bb7e6269cc0e4ac96299e9cae95c0071a7ab16
poriyasohrabi7575-del <poriya.sohrabi7575@gmail.com>
2026-07-31 17:18:39 +0330

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json([
     {
-      id: 1,
+      id: "1",
       name: "لباس دخترانه",
       price: 450000,
       image: "/images/test.jpg"
